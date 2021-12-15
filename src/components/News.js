0e0f07f6f78ccc306a9d2e94bc9faf16e954679f
Kunal Bafna <bafnakunal3@gmail.com>
2021-12-15 16:55:37 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -2,26 +2,69 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+    // constructor inside which we have to use state
     constructor(){
         super();
         console.log("Hello I am a constructor from News component");
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page:1
         }
     }
 
+    // fetching data from the api
     // async runs the func and waits for the promises to resolve =  that is await
+    // this is a lifecycle method which runs after the render method
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=1c31506a7075446a856cad4cc0edb939"
+        console.log("Hello I am a comp from News component");
+        
+        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=1c31506a7075446a856cad4cc0edb939&page=1&pageSize=20"
 
         // fetching api
         let data = await fetch(url);
         let parsedData = await data.json()
-        this.setState({articles: parsedData.articles})
+        // setting the state
+        this.setState({articles: parsedData.articles,
+            totalResults: parsedData.articles.totalResults})
+    }
+
+    handlePreviousClick = async () =>{
+
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=1c31506a7075446a856cad4cc0edb939&page=${this.state.page - 1}&pageSize=20`;
+
+        // fetching api
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        // setting the state
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles})
+       
+
+    }
+    
+    handleNextClick = async () =>{
+
+        // Math.ceil returns the next integer ex- 4.6 is the value then it will return 5
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }else{   
+
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=1c31506a7075446a856cad4cc0edb939&page=${this.state.page + 1}&pageSize=20`;
+
+        // fetching api
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        // setting the state
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles})
+        }
     }
 
     render() {
+        console.log("Hello I am a render from News component");
         return (
             <div className="container my-3">
                 <h2>NewsMonkey - Top Headlines</h2>
@@ -34,6 +77,10 @@ export class News extends Component {
                     )
                 })}
                 </div> 
+                <div className="container d-flex justify-content-between">
+                <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePreviousClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    </div>
             </div>
         )
     }
